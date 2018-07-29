bb971a41a878fcbc5cc63ff26c9be90691c1c650
anotherFeng <regnagleppod@gmail.com>
2018-07-28 21:00:46 -0700

    create navbar and table to show jobs data

--- client/app/jobs/job-list/job-list.component.ts
+++ client/app/jobs/job-list/job-list.component.ts
@@ -8,7 +8,36 @@ import { Job } from '../job.model';
   styleUrls: ['./job-list.component.css']
 })
 export class JobListComponent implements OnInit {
-  jobs: Job[] = [];
+  jobs: Job[] = [
+    new Job(
+      "Google",
+      "Senio Dev",
+      "120,000",
+      null,
+      null,
+      null,
+      null,
+      "accepted",
+      "none",
+      "John Doe",
+      "64621312345",
+      "john.doe@google.com"
+    ),
+    new Job(
+      "Google",
+      "Senio Dev",
+      "120,000",
+      null,
+      null,
+      null,
+      null,
+      "accepted",
+      "none",
+      "John Doe",
+      "64621312345",
+      "john.doe@google.com"
+    )
+  ];
 
   constructor() { }
 
--- client/app/jobs/job.model.ts
+++ client/app/jobs/job.model.ts
@@ -1,6 +1,6 @@
 export class Job {
   public company: String;
-  public jobTitle: String;
+  public job: String;
   public compensation: String;
   public appliedDate: Date;
   public phoneScreenDate: Date;
@@ -12,13 +12,13 @@ export class Job {
   public contactPhone: String;
   public contactEmail: String;
 
-  constructor(company: String, jobTitle: String, compensation: String, 
+  constructor(company: String, job: String, compensation: String, 
     appliedDate: Date, phoneScreenDate: Date, onSiteDate: Date,
     offerDate: Date, result: String, obstacles: String, 
     mainContact: String, contactPhone: String, contactEmail: String){
 
       this.company = company;
-      this.jobTitle = jobTitle;
+      this.job = job;
       this.compensation = compensation;
       this.appliedDate = appliedDate;
       this.phoneScreenDate = phoneScreenDate;
